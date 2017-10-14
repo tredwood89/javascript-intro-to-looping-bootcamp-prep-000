5c6133fe6f7a3ca06066e9697ae0650992d08474
tredwood89 <tredwood89@gmail.com>
2017-10-14 00:25:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,10 +4,10 @@ function forLoop (array) {
 
   if (i < 25 )  {
 
-    array.push "I am ${i} strange loops"
+    array.push `I am ${i} strange loops`
 
   } else  if (i === 1 ) {
 
-    array.push "I am 1 strange loop"
+    array.push `I am 1 strange loop`
  }
 }
