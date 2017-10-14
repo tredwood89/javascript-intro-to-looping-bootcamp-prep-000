2a64699ab40c106e49a4ce7abed8b757a655f4de
tredwood89 <tredwood89@gmail.com>
2017-10-14 02:14:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,3 +23,20 @@ function whileLoop(n) {
 
   return 'done'
 }
+
+
+ function doWhileLoop(array) {
+   
+   function maybeTrue() {
+     return Math.random() >= 0.5
+}
+
+  do {
+    array.pop 
+} while array.length > 0 && maybeTrue()
+  }
+
+  return array
+ }
+
+ 
